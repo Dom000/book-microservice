3dc7585007c5844ee9eb800d28320d23fb26e2ac
dom000 <godknowsegiboy00@gmail.com>
2023-09-11 12:59:19 +0100

    latest

--- bookstore-client/components/Header/Header.tsx
+++ bookstore-client/components/Header/Header.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { BiSolidUserCircle } from "react-icons/bi";
 
 const Header: React.FC = () => {
   return (
-    <div className="flex flex-row shadow-md p-3 md:p-7 items-center  justify-center space-x-10 md:space-x-16">
+    <div className="flex flex-row shadow-md absolute top-0 w-full  p-3 md:p-6 items-center  justify-center space-x-5 md:space-x-16">
       <Link to={"/"}>
         <div className="hover:bg-slate-400/25 cursor-pointer p-1 rounded-md">
           <p className="font-bold text-gray-900 text:base md:text-lg">Users</p>
@@ -20,6 +21,9 @@ const Header: React.FC = () => {
             Create Books
           </p>
         </div>
+      </Link>
+      <Link to={"/userDetails"}>
+        <BiSolidUserCircle className="text-3xl text-gray-400" />
       </Link>
     </div>
   );
--- bookstore-client/src/pages/User/UserDeatils.tsx
+++ bookstore-client/src/pages/User/UserDeatils.tsx
@@ -1,12 +1,27 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { useAuthenticateStore } from "../../../features/store";
+import { PiUserBold } from "react-icons/pi";
 
 const UserDetails: React.FC = () => {
+  const userDetails = useAuthenticateStore((state) => state);
+
   return (
-    <div className="flex ">
-      <div className="w-2/5">Side bar</div>
-      <div className="w-full">
-        <Outlet />
+    <div className="md:flex flex-col ">
+      <div className="md:w-1/5 w-full rounded-md shadow-md h-80 flex-col flex  items-center  p-3">
+        <PiUserBold className="text-[100px] text-gray-400" />
+        <div className="w-4/5 flex flex-col my-2 items-left space-y-3">
+          <div className="flex space-x-5">
+            <p className="font-bold">Name:</p>
+            <p>{userDetails.userDetails.user_name}</p>
+          </div>
+          <div className="flex space-x-5 flex-wrap">
+            <p className=" text-sm">{userDetails.userDetails.email}</p>
+          </div>
+          <div className="flex space-x-5">
+            <p className="font-bold text-sm">No of Books:</p>
+            <p className=" text-sm">John Doe</p>
+          </div>
+        </div>
       </div>
     </div>
   );
